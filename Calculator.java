028fd873796d37182fd729374b7f7a8f2cf5c74f
Arturo-Molina <37124369+Arturo-Molina@users.noreply.github.com>
2018-11-26 17:40:24 -0800

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -4,21 +4,13 @@ public class Calculator {
 	public static final int HEIGHT = 300 + 20; //20 hardcoded in as the height of window bar
 	public static final int WIDTH = 300;
 	Window window;
-	boolean running = true;
 	
 	public Calculator(){
-		window = new Window(WIDTH,HEIGHT); 
-		
-		
-		
-		
-		
+		window = new Window(WIDTH,HEIGHT); 	
 	}
+	
 	public static void main(String[] args) {
-		
-		
 		new Calculator();
-
 	}
 	
 	
